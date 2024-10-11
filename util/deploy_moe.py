2327eb48e7a76e16884a8ed15cfc6cc9c690acd2
Paulo Lacerda <pclacerda@gmail.com>
2024-10-11 10:09:53 -0300

    enable the chat interface

--- util/deploy_moe.py
+++ util/deploy_moe.py
@@ -75,13 +75,13 @@ def deploy_flow(endpoint_name, deployment_name):
         name=deployment_name,
         endpoint_name=endpoint_name,
         model=Model(
-            name="rag_flow",
+            name="ragflow",
             path=flow_path,  # path to promptflow folder
             properties=[ # this enables the chat interface in the endpoint test tab
-                ["azureml.promptflow.source_flow_id", "basic-chat"],
+                ["azureml.promptflow.source_flow_id", "ragflow"],
                 ["azureml.promptflow.mode", "chat"],
-                ["azureml.promptflow.chat_input", "chat_input"],
-                ["azureml.promptflow.chat_output", "reply"]
+                ["azureml.promptflow.chat_input", "question"],
+                ["azureml.promptflow.chat_output", "answer"]
             ]
         ),
         environment=Environment(
